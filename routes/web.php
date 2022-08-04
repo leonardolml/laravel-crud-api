9545e57b3306395c9a2062b2fecdb3c532fe10c4
Leonardo Matos <leonardolml@gmail.com>
2022-08-03 23:25:18 -0300

    Web route added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemWebController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('items')->name('items.')->group(function () {
+    Route::get('', [ ItemWebController::class, 'all' ])->name('all');
 });
