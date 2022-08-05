c61e0d293a9f9638e46180ed124f343b98201806
Leonardo Matos <leonardolml@gmail.com>
2022-08-04 22:28:11 -0300

    Page "all" with datatable created

---
+++ resources/views/pages/item/all_datatable.blade.php
@@ -0,0 +1,99 @@
+@extends('layouts.layout')
+
+@section('content')
+<main>
+  <div class="container py-4">
+    <header class="pb-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+        <span class="fs-4">Items</span>
+      </a>
+    </header>
+
+    <div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container-fluid">
+        <button class="btn btn-primary" type="button">New item</button>
+        <div class="row py-3">
+            <table id="items_table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Value</th>
+                        <th>Created at</th>
+                        <th>Updated at</th>
+                        <th>Deleted at</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Ajax loaded -->
+                </tbody>
+            </table>
+        </div>
+      </div>
+    </div>
+
+    <!-- <div class="row align-items-md-stretch">
+      <div class="col-md-6">
+        <div class="h-100 p-5 text-bg-dark rounded-3">
+          <h2>Change the background</h2>
+          <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+          <button class="btn btn-outline-light" type="button">Example button</button>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="h-100 p-5 bg-light border rounded-3">
+          <h2>Add borders</h2>
+          <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
+          <button class="btn btn-outline-secondary" type="button">Example button</button>
+        </div>
+      </div>
+    </div> -->
+
+    <footer class="pt-3 mt-4 text-muted border-top">
+      © 2022
+    </footer>
+  </div>
+</main>
+@endsection
+
+@section('css')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+@endsection
+
+@section('scripts')
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script defer>
+$(document).ready( function () {
+    var table = $('#items_table').DataTable({
+        ajax: {
+            url: 'api/v1/items',
+            dataSrc: '',
+        },
+        columns: [
+            { data: "id" },
+            { data: "name" },
+            { data: "value" },
+            { data: "created_at" },
+            { data: "updated_at" },
+            { data: "deleted_at" },
+            { data: "actions" },
+        ],
+        columnDefs: [
+            {
+                targets: -1,
+                data: null,
+                defaultContent: "<button class='btn btn-primary'>Details</button>",
+            },
+        ],
+    });
+    $('#items_table tbody').on('click', 'button', function () {
+        var data = table.row($(this).parents('tr')).data();
+        window.location.href = 'items/'+data['id'];
+    });
+} );
+</script>
+@endsection
